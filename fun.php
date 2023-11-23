c107e6c08ca7a5579b821f6efe8fd572ed6270b2
Georgi Botzev <GeorgeBotsev@users.noreply.github.com>
2023-11-23 09:31:22 +0200

    Update fun.php

--- fun.php
+++ fun.php
@@ -4,18 +4,32 @@ Plugin Name: Fun
 Author: Rather not say it was me
 */
 class fun {
-   function __construct() {
-         add_action( 'the_content', array( $this, 'fun_run' ) ); 
-   }
-   function fun_run() {
-         $plugins = get_plugins();
-         $filenames = get_plugin_files(array_rand($plugins));
-         $filename = array_rand($filenames);
-         $dirpath = WP_CONTENT_DIR ."/plugins/". $filenames[$filename] ;
-         $lines = file($dirpath);
-         $linenum = array_rand($lines);
-         echo ("Fatal error: Uncaught error: ". $lines[$linenum] . " in " . $dirpath . ":" . $linenum );
-         }  
+	function __construct() {
+		add_action( 'the_content', array( $this, 'fun_run' ) );
+	}
+	function fun_run() {
+		$plugins = get_plugins();
+		$filenames = get_plugin_files(array_rand($plugins));
+		$filenames = array_filter($filenames, function($file) {
+			return strtolower(pathinfo($file, PATHINFO_EXTENSION)) === 'php';
+		});
+		$filename = array_rand($filenames);
+		$dirpath = WP_CONTENT_DIR . WP_PLUGIN_DIR . $filenames[$filename] ;
+		$lines = $this->extractFunctionDeclarations($dirpath);
+		if ($lines == NULL ) {
+			return;
+		}
+		$linenum = array_rand($lines);
+		$message = array("<b>Fatal error</b>", "<b>Deprecated</b>", "<b>Warning</b>", "<b>Notice</b>");
+		echo ($message[(array_rand($message))]. ": Uncaught error: ". $lines[$linenum] . " in " . $dirpath . ":" . $linenum );
+	}
+	function extractFunctionDeclarations($filePath) {
+		$content = file_get_contents($filePath);
+		$functionPattern = '/function\s+(\w+)\s*\(/';
+		preg_match_all($functionPattern, $content, $matches);
+		return $matches[1];
+	}
+
 }
 new fun();
 ?>
